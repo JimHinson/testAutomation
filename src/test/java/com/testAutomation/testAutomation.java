d697cbe6bc72f28187bed711df8e8c2c32f8ad0f
Jim Hinson <Jim.Hinson@live.com>
2020-08-14 20:22:44 -0400

    Getting started

---
+++ src/test/java/com/testAutomation/testAutomation.java
@@ -0,0 +1,15 @@
+package com.testAutomation;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class testAutomation {
+
+    @Test
+    public void testAutomation() {
+
+    }
+}
